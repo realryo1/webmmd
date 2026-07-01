1ab47f1c7d7481e2b04e00b407ccae2731102f68
realryo1 <realryo1@gmail.com>
2026-07-01 13:07:53 +0900

    PWA修正

--- vite.config.js
+++ vite.config.js
@@ -26,13 +26,13 @@ export default defineConfig({
         lang: "ja",
         icons: [
           {
-            src: "./icons/icon-192.svg",
+            src: "icons/icon-192.svg",
             sizes: "192x192",
             type: "image/svg+xml",
             purpose: "any maskable"
           },
           {
-            src: "./icons/icon-512.svg",
+            src: "icons/icon-512.svg",
             sizes: "512x512",
             type: "image/svg+xml",
             purpose: "any maskable"
